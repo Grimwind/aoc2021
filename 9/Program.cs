ebc6456b0588de49d00b32cdd234cce036101e7d
Piotr Wiśniewski <piotr.wisniewski@dgcone.pl>
2021-12-16 21:43:55 +0100

    Day9 part 2

--- 9/Program.cs
+++ 9/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace _9
 {
@@ -32,34 +33,32 @@ namespace _9
 
                 for (int x = 0; x < data.GetLength(0); x++)
                 {
-                    
-
                     if (data[x, y] < previous_x)
-                    {    
+                    {
                         low_x = x;
                     }
                     if (data[x, y] > previous_x)
                     {
-                        low_points[low_x, y]=1;
+                        low_points[low_x, y] = 1;
                     }
                     previous_x = data[x, y];
                 }
-                if (low_x == max_x-1)
+                if (low_x == max_x - 1)
                 {
                     low_points[low_x, y] = 1;
                 }
             }
 
-           // DrawMap(low_points);
+            // DrawMap(low_points);
             //Console.WriteLine();
-            
+
             for (int x = 0; x < data.GetLength(0); x++)
             {
                 int previous_y = 9;
                 int low_y = -1;
                 for (int y = 0; y < data.GetLength(1); y++)
                 {
-                    
+
                     if (data[x, y] < previous_y)
                     {
                         low_y = y;
@@ -76,24 +75,74 @@ namespace _9
                 }
             }
 
+            List<int> basinSizes = new List<int>();
             int risk_level = 0;
             for (int y = 0; y < data.GetLength(1); y++)
             {
                 for (int x = 0; x < data.GetLength(0); x++)
                 {
-                    if (low_points[x,y] == 2)
+                    if (low_points[x, y] == 2)
                     {
                         risk_level += 1 + data[x, y];
+
+                        var creep = new int[max_x, max_y];
+                        var size = Creep(x, y, data, ref creep);
+
+                        // Console.WriteLine();
+                        // DrawMap(creep);
+                        // Console.WriteLine($"Size: {size}");
+
+                        basinSizes.Add(size);
                     }
                 }
-                
+
             }
 
             //DrawMap(data);
             //Console.WriteLine();
             //DrawMap(low_points);
 
-            Console.WriteLine($"Result: {risk_level}");
+            var sizesSorted = basinSizes.OrderByDescending(x=> x).ToList();
+
+            var result = sizesSorted[0]*sizesSorted[1]*sizesSorted[2];
+
+            Console.WriteLine($"Result: {risk_level}; Result 2: {result}");
+        }
+
+        public static int Creep(int x, int y, int[,] map, ref int[,] creep)
+        {
+            // Console.WriteLine($"Creeping for {x} ; {y}");
+            // We're out of map
+            if (x < 0 || x >= map.GetLength(0) || y < 0 || y >= map.GetLength(1))
+            {
+                // Console.WriteLine($"Out of map");
+                return 0;
+            }
+            // We hit the ridge
+            if (map[x, y] == 9)
+            {
+                // Console.WriteLine($"Hit the 9");
+                return 0;
+            }
+
+            // We already calculated this field
+            if (creep[x, y] == 1)
+            {
+                // Console.WriteLine($"Been here, done that");
+                return 0;
+            }
+
+            var score = 1;
+            creep[x,y] = 1;
+
+            score += Creep(x+1, y, map, ref creep);
+            score += Creep(x-1, y, map, ref creep);
+            score += Creep(x, y+1, map, ref creep);
+            score += Creep(x, y-1, map, ref creep);
+
+            // Console.WriteLine($"Collected score of: {score}");
+
+            return score;
         }
 
         public static void DrawMap(int[,] map)
